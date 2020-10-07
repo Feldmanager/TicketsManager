bc552a42ce4071b19a0ad1f4ea3e7bbfd2a63d2a
citizen6 <theLittleCitizen6@gmail.com>
2020-10-07 21:58:00 +0300

    added cors using

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const ticketRouter = require("./Routes/TicketRoutes.js");
 const ticketsRouter = require("./Routes/TicketsRouter.js");
-
+const cors = require("cors")
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+  }
+  
 const app = express();
-
+  
+app.use(cors(corsOptions));
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 
