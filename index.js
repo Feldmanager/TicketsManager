11caeff1bde438b3c86a8ce2806ed5541635768a
root <root@FeldManager-Prod01.hwwhxjbuclnu1gp1towbb0heee.ax.internal.cloudapp.net>
2020-10-14 22:38:27 +0000

    add https

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const commentRouter = require('./Routes/CommentRouter.js');
 const commentsRouter = require('./Routes/CommentsRouter.js');
 const {Authorize,ValidateUser}= require('commonframework');
 const cors = require("cors")
+const fs = require('fs');
+const https = require('https');
+const privateKey  = fs.readFileSync('/run/secrets/server.key', 'utf8');
+const certificate = fs.readFileSync('/run/secrets/server.crt', 'utf8');
+
+let credentials = {key: privateKey, cert: certificate};
+
 var corsOptions = {
     origin: [
-      "http://localhost:3001",
-      "http://10.1.0.27:3001",
-      "http://10.1.0.26:3001",
-      "http://10.1.0.17:3001",
-      "http://10.1.0.19:3001",
-      "http://10.1.0.14:3001",
-      "http://10.1.0.11:3001",
-      "http://10.1.0.119:3000"
-  ],
-    optionsSuccessStatus: 200
+            "http://40.118.47.110",
+            "http://www.feldmanager.com",
+            "https://40.118.47.110",
+            "https://www.feldmanager.com",
+            "https://www.feldmanager.com:3001",
+            "https://40.118.47.110:3001",
+            "https://www.feldmanager.com:3002",
+            "https://40.118.47.110:3002",
+            "https://www.feldmanager.com:3003",
+            "https://40.118.47.110:3003",
+            "https://www.feldmanager.com:3004",
+            "https://40.118.47.110:3004"
+        ]
   }
   
 const app = express();
@@ -49,5 +59,6 @@ app.get('/sainitys',async(req, res) =>{
   res.status(200).send();
 })
 
+let httpsServer = https.createServer(credentials, app);
 
-let server = app.listen(3000, () => console.log('Listening on port 3000...'));
+let server = httpsServer.listen(3000, () => console.log('Listening on port 3000...'));
